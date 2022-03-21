5396bd5a0a0cd75ca93aef0b72ff1cbe73b6419d
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-22 02:02:19 +0300

    son

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,27 +22,108 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranGenisligi = ekranBilgisi.size.width;
+    final double ekranYuksekligi = ekranBilgisi.size.height;
     return Scaffold(
+      backgroundColor: Colors.deepPurple,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
-
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: ekranYuksekligi / 20),
+                child: SizedBox(
+                    width: ekranGenisligi / 4,
+                    child: Image.asset("images/logo.png")),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: ekranGenisligi / 20,
+                    top: ekranYuksekligi / 40,
+                    right: ekranGenisligi / 20,
+                    bottom: ekranYuksekligi / 40),
+                child: SizedBox(
+                  width: ekranGenisligi / 1,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: "Kullanici Adi",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: ekranGenisligi / 20,
+                    top: ekranYuksekligi / 40,
+                    right: ekranGenisligi / 20,
+                    bottom: ekranYuksekligi / 40),
+                child: SizedBox(
+                  width: ekranGenisligi / 1,
+                  child: const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Sifre",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: ekranGenisligi / 1.9,
+                child: ElevatedButton(
+                  onPressed: () {
+                    print("Giris Yapildi");
+                  },
+                  child: Text(
+                    "Giris Yap",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ekranGenisligi / 25,
+                        color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.redAccent,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: GestureDetector(
+                  onTap: () {
+                    print("Yardim Secildi");
+                  },
+                  child: Text(
+                    "Yardim",
+                    style: TextStyle(
+                        fontSize: ekranGenisligi / 30,
+                        color: Colors.greenAccent,
+                        decoration: TextDecoration.underline,
+                        letterSpacing: 2),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      
     );
   }
 }
